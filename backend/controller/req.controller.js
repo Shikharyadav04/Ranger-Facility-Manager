e2a703b785bc5d67a4cc201c02575b1be5f9acd7
Prince Yadav <princeyjnv12345@gmail.com>
2025-12-05 03:31:03 +0530

    add status request and domain request

--- backend/controller/req.controller.js
+++ backend/controller/req.controller.js
@@ -1,6 +1,6 @@
 import {Request} from "../models/request.model.js";
 
-
+// API for generate request 
 export const generate_request = async(req,res)=>{
     
     try{
@@ -42,6 +42,7 @@ export const generate_request = async(req,res)=>{
     
 }
 
+// API for getting all requests
 export const getAll_request = async(req,res)=>{
      
     try{
@@ -64,6 +65,7 @@ export const getAll_request = async(req,res)=>{
     }
 }
 
+// API for get all low_priority requests
 export const getLow_priority = async(req , res )=>{
        try{
             const easyRequests = await Request.find({ priority: "Low" });
@@ -83,6 +85,7 @@ export const getLow_priority = async(req , res )=>{
        }
 }
 
+// API for get all medium_priority requests
 export const getMedium_priority = async(req , res )=>{
        try{
             const mediumRequests = await Request.find({ priority: "Medium" });
@@ -102,6 +105,7 @@ export const getMedium_priority = async(req , res )=>{
        }
 }
 
+// Api for get all high_priority requests 
 export const getHigh_priority = async(req , res )=>{
        try{
             const highRequests = await Request.find({ priority: "High" });
@@ -120,3 +124,158 @@ export const getHigh_priority = async(req , res )=>{
             });
        }
 }
+
+
+// API  Requests assigned to a particular engineer.
+export const getAssignedEngineer_Request = async(req, res )=>{
+    
+    try{
+        const engineerId = req.user_id;
+
+        const assignedRequests = await Request.find({
+        assigned_engineering_id: engineerId
+        });
+
+        return res.status(200).json({
+        success: true,
+        count: assignedRequests.length,
+        data: assignedRequests
+        });
+    }
+    catch(err){
+        console.log("Assigned Engineer Request Error:", err.message);
+
+        return res.status(500).json({
+        success: false,
+        error: "Internal Server Error"
+        });
+    }
+    
+}
+
+// API for get status request 
+export const getCompleted_Request = async(req, res )=>{
+     
+    try{
+         const Completed_request = await Request.find({curr_status: "Resolved"});
+         
+         return res.status(200).json({
+            success : true ,
+            count : Completed_request.length,
+            data : Completed_request 
+         });
+    }
+    catch(err){
+        console.log("Completed Request Error :" , err.message);
+
+        return res.status(500).json({
+            success : false ,
+            error : "Internal server Error "
+
+        })
+    }
+     
+    
+
+}
+
+
+export const getInprogess_Request = async(req, res)=>{
+     
+    try{
+        const Inprogess_Request = await Request.find({ curr_status : "Inprogess"});
+
+        return res.status(200).json({
+            status : true , 
+            count : Inprogess_Request.length,
+            data : Inprogess_Request 
+        })
+    }
+    catch(err){
+        console.log("get Inprogress Error: ", err.message);
+        
+        return res.status(500).json({
+            success : false ,
+            error : "Internal server error "
+        })
+    }
+}
+
+// Api for get all  new created request , but no engineer has been assigned yet
+export const getOpen_Request = async(req, res)=>{
+    try{
+        const Open_request = await Request.find({curr_status : "Open"});
+        
+        return res.status(200).json({
+            success : true , 
+            count : Open_request.length , 
+            data : Open_request
+        })
+    }
+    catch(err){
+        console.log("get Open request Error :" , err.message);
+        return res.status(500).json({
+            success : false,
+            error : "Internal server Error "
+        })
+    }
+}
+
+// Api for getting  domain request
+export const getElectrical_Request = async(req, res)=>{
+    try{
+        const Electrical_request = await Request.find({engineering_domain : "electrical"});
+        
+        return res.status(200).json({
+            success : true , 
+            count : Electrical_request.length , 
+            data : Electrical_request
+        })
+    }
+    catch(err){
+        console.log("get electrical domain request Error :" , err.message);
+        return res.status(500).json({
+            success : false,
+            error : "Internal server Error "
+        })
+    }
+}
+
+export const getMechanical_Request = async(req, res)=>{
+    try{
+        const Mechanical_request = await Request.find({engineering_domain : "mechanical"});
+        
+        return res.status(200).json({
+            success : true , 
+            count : Mechanical_request.length , 
+            data : Mechanical_request
+        })
+    }
+    catch(err){
+        console.log("get Mechanical domain request Error :" , err.message);
+        return res.status(500).json({
+            success : false,
+            error : "Internal server Error "
+        })
+    }
+}
+
+export const getComputer_Science_Request = async(req, res)=>{
+    try{
+        const Computer_Science = await Request.find({engineering_domain : "computer Science"});
+        
+        return res.status(200).json({
+            success : true , 
+            count : Computer_Science.length , 
+            data : Computer_Science
+        })
+    }
+    catch(err){
+        console.log("get Mechanical domain request Error :" , err.message);
+        return res.status(500).json({
+            success : false,
+            error : "Internal server Error "
+        })
+    }
+}
+
